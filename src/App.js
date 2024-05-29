ef11fe9ed389d00153882b7fe790441f0be2c7e8
rawvie <rawvie@ngit.com.np>
2024-05-29 16:34:20 +0545

    all commits batman

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Lucid, Blockfrost } from "lucid-cardano";
+import { useState } from "react";
 
 function App() {
+  const [cbor, setCbor] = useState("");
+
+  const signTransaction = async () => {
+    const lucid = await Lucid.new(
+      new Blockfrost(
+        "https://cardano-preview.blockfrost.io/api/v0",
+        "previewWenCzTRwLmYSJuTHiZv5yiqdkJ7l2Iih"
+      ),
+      "Preview"
+    );
+    const api = await window.cardano.nami.enable();
+    lucid.selectWallet(api);
+    const unsigned = await lucid
+      .newTx()
+      .payToAddress(
+        "addr_test1qz4ez3z8qte2yxvq3zv22cdw27peen83dx6cxl64de2zw6sjuj26mwkl3nnal9g0e9yqlnh6jux7eswrduc0jpm6fa7qrsc2m5",
+        { lovelace: 200000000n }
+      )
+      .complete();
+    const signed = await unsigned.sign().complete();
+    const hash = await signed.submit();
+    setCbor(hash);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={signTransaction}>Sign transaction</button>
+      <br/>
+      <textarea value={cbor}/>
     </div>
   );
 }
